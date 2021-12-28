0192f4d391003354a95ca42f9449bdf9f8345bd8
Chijioke Ugwuanyi <Chijioke.Ugwuanyi@Interswitchng.com>
2021-12-28 11:32:15 +0100

    Added transactions list.

--- src/AccountActions.java
+++ src/AccountActions.java
@@ -1,28 +1,54 @@
+import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 class Transactions {
+    LocalDateTime date;
+    double amount;
+    String type;
 
+    public Transactions(LocalDateTime date, double amount, String type) {
+        this.date = date;
+        this.amount = amount;
+        this.type = type;
+    }
 }
 
 public class AccountActions {
     double balance;
     String customerName;
     int accountId;
+    ArrayList<Transactions> transactions = new ArrayList<>();
 
     public AccountActions(String cName, int aId) {
         this.accountId = aId;
         this.customerName = cName;
     }
 
+    void displayTxns(LocalDateTime date, double amount, String type) {
+        System.out.println("==============================");
+        System.out.println("Transaction date: " + date);
+
+        System.out.println("Transaction amount: " + amount);
+
+        System.out.println("Transaction type: " + type);
+        System.out.println("===============================");
+    }
+
     void makeDeposit(double amount) {
         if(amount != 0) {
             this.balance += amount;
+            LocalDateTime newDate = LocalDateTime.now();
+            this.transactions.add(new Transactions(newDate, amount, "credit"));
         }
     }
 
     void makeWithdrawal(double amount) {
         if(amount != 0 ) {
             this.balance -= amount;
+
+            LocalDateTime newDate = LocalDateTime.now();
+            this.transactions.add(new Transactions(newDate, amount, "debit"));
         }
     }
 
@@ -85,15 +111,18 @@ public class AccountActions {
                     System.out.println("Enter amount to withdraw:");
 
                     double withdrawAmount = optionScanner.nextDouble();
-                    this.makeDeposit(withdrawAmount);
+                    this.makeWithdrawal(withdrawAmount);
 
                     System.out.println("Your balance is now " + this.balance);
                     System.out.println("================================");
 
                     break;
                 case 'D':
-                    System.out.println("===============================");
-                    System.out.println("Coming soon...");
+
+                    for(Transactions txn : transactions) {
+                        displayTxns(txn.date, txn.amount, txn.type);
+                        System.out.println();
+                    };
 
                     break;
 
